9623a79f6ef0b007777ede1ca3101b273aad5abc
Yampier Ponce <developer.yampier@gmail.com>
2021-12-15 14:03:57 -0500

    ✨ mi primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Conversor de dolares a soles
+
+practicando habilidades de TypeScript básicos para entender los fundamentos necesarios.
+
+<cite> 👦 Yampier Ponce </cite>
--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-const $ = (selector) => document.querySelector(selector);
+const DOM = (selector) => document.querySelector(selector);
 
-const $formulario = $("#formulario");
-const $monto = $("#monto");
-const $resultado = $("#resultado");
+const formularioDom = DOM("#formulario");
+const montoDom = DOM("#monto");
+const resultadoDom = DOM("#resultado");
 
-const DOLAR: number = 4.04;
+const dolar: number = 4.04;
 
-function Validacion(e: Event) {
+const validacion = (e: Event) => {
   e.preventDefault();
   if ($monto.value == "") return false;
-  const monto = Number($monto.value);
+  const monto = Number(montoDom.value);
   const resultado: number = DOLAR * monto;
-  $resultado.textContent = `El monto en soles es S/ ${resultado}`;
-}
+  resultadoDom.textContent = `El monto en soles es S/ ${resultado}`;
+};
 
-$formulario.addEventListener("submit", Validacion);
+$formulario.addEventListener("submit", validacion);
